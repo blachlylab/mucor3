b95c4574dccc7195888ed3d66c3a8083690263dd
kekananen <kekananen@gmail.com>
2022-02-16 10:58:33 -0500

    Added multiple column input

--- mucor3/utils/sample_indexer.py
+++ mucor3/utils/sample_indexer.py
@@ -11,7 +11,7 @@ import pandas as pd
 import argparse as ap
 import numpy as np
 
-def __readInFile(path):
+def readInFile(path):
     """ Reads in a file object to a pandas' frame.
     file is assumed to have a single header row.
     Input:
@@ -29,7 +29,7 @@ def __readInFile(path):
         frame=pd.read_csv(path)
     return frame
 
-def __expandFrameOnDelimiter(frame, column):
+def expandFrameOnDelimiter(frame, column):
     """ Splits file rows by a ';' delimiter
     Input:
         frame - the pandas frame to split
@@ -42,18 +42,22 @@ def __expandFrameOnDelimiter(frame, column):
     # Convert the string to a list by taking a dictionary of key-value pairs and
     # unpacking into keyword arguments in a function call. The frame's internals 
     # are thus split by the ';' character.
-    listColumn = frame.assign(**{column:frame[column].str.split(';')})
-    # Created a vectorized funcion which uses numpy's repeat. This function to 
-    # transform each element of a list-like to a row, replicating index values. 
-    # Indexes will be duplicated for the rows that are expanded.
-    # Todo - Convert to explode pandas function later since it's easier to read
-    expandedFrame = pd.DataFrame({
-                        col:np.repeat(listColumn[col].values, listColumn[column].str.len())
-                        for col in listColumn.columns.difference([column])
-                    }).assign(**{column:np.concatenate(listColumn[column].values)})[listColumn.columns.tolist()]
+    expandedFrame = frame
+    if not type(column) is list:
+        column = [column]
+    for c in column:
+        listColumn = expandedFrame.assign(**{c:frame[c].str.split(';')})
+        # Created a vectorized funcion which uses numpy's repeat. This function to 
+        # transform each element of a list-like to a row, replicating index values. 
+        # Indexes will be duplicated for the rows that are expanded.
+        # Todo - Convert to explode pandas function later since it's easier to read
+        expandedFrame = pd.DataFrame({
+                            col:np.repeat(listColumn[col].values, listColumn[c].str.len())
+                            for col in listColumn.columns.difference([c])
+                        }).assign(**{c:np.concatenate(listColumn[c].values)})[listColumn.columns.tolist()]
     return expandedFrame
 
-def __replaceValuesWithDelimiter(rawFile, column, accName, index):
+def replaceValuesWithDelimiter(rawFile, column, accName, index):
     """ Converts all ID on the same line. This retains the 
     condensed format of a file if desired.
     Input: 
@@ -97,11 +101,11 @@ def main():
     parser.add_argument("--index","-i", type=str, required=True,
         help="File containing the column with a set of indentifers to be linked")
     parser.add_argument("--column","-c", type=str, required=False,
-                help="Name of column that the ids exist in the input file")
+                help="Name of column/s that the ids exist in the input file")
     parser.add_argument("--idName","-in", type=str, required=False,
         help="Name of column that the ids exist in the index file")
     parser.add_argument("--accName","-an", type=str, required=False,
-        help="Name of column that Accessions exist in the index file")
+        help="Name of column/s that Accessions exist in the index file")
     parser.add_argument("--output","-o", type=str, required=True,
         help="Name of file to be output")
     parser.add_argument("--split","-s", type=str, required=False, default="Flase",
@@ -109,40 +113,66 @@ def main():
     args = parser.parse_args()
     
     # Read files intp pandas dataframe
-    index = __readInFile(args.index)
-    rawFile = __readInFile(args.file)
+    index = readInFile(args.index)
+    rawFile = readInFile(args.file)
     if not args.column is None:
         # Get the column that has the sample ID in them
         columnFrame = rawFile[args.column]
-        # Rename the column for the index to match
-        index.rename(columns = {args.idName:args.column}, inplace = True)
+        idNameLST = args.idName.split(',')
+        accNameLST = args.accName.split(',')
         # explode the semicolon seperators onto new lines. The assign function
         # allows for the columns with a ';' to be converted into a list and then
         # exploded out onto different lines.
-        expandedIndex = __expandFrameOnDelimiter(index, args.accName)
+        expandedIndex = expandFrameOnDelimiter(index, accNameLST)
+        # Rename the columns for the index to match so that they may be combined below if
+        # if they're are multiple columns being used for the index row.
+        for idName in idNameLST:
+            expandedIndex.rename(columns = {idName:args.column}, inplace = True)
+        for accName in accNameLST:
+            expandedIndex.rename(columns = {accName:"accession"}, inplace = True)
+        # De-duplicates the columns by adding a unique id then stack the data. 
+        if len(idNameLST) > 1 or len(accNameLST) > 1:
+            expandedIndex = (expandedIndex.set_axis(pd.MultiIndex.from_arrays([expandedIndex.columns,
+                                                                                expandedIndex.groupby(level=0, axis=1).cumcount()
+                                                                                ]), axis=1)
+                                .stack(level=1)
+                                .sort_index(level=1)
+                                .droplevel(1)
+                                .drop_duplicates(subset=expandedIndex.columns[expandedIndex.columns.duplicated()])
+                            )
+        expandedIndex = expandedIndex[[args.column, "accession"]]
+
         expandedRaw = rawFile
         if args.split == "True":
-            expandedRaw = __expandFrameOnDelimiter(rawFile, args.column)
-            # Merge the data frames together and convert the series into dataframe
+            expandedRaw = expandFrameOnDelimiter(rawFile, args.column)
+            # Merge the data frames together and convert the series into dataframe    
             bigDataFile = pd.merge(expandedIndex, expandedRaw, on=args.column)
-            bigDataFile[args.column] = bigDataFile[args.accName]
-            # May need to drop all header columns but args.idName here to get same file out
-            out = bigDataFile.drop(columns=[args.accName])
+            bigDataFile[args.column] = bigDataFile["accession"]
+            out = bigDataFile.drop(columns=["accession"])
             # Write out the file
             out.to_csv(args.output, index=False, sep='\t')
         else:
-            expandedRaw = __replaceValuesWithDelimiter(rawFile, args.column, args.accName, expandedIndex)
+            replaceValuesWithDelimiter(rawFile, args.column, "accession", expandedIndex)
             expandedRaw.to_csv(args.output, index=False, sep='\t')
     else:
         # No column name provided
         print("No column name was provided to filter on, assuming values in header")
+        idNameLST = args.idName.split(',')
+        accNameLST = args.accName.split(',')
         header = [x for x in list(rawFile.columns)]
-        samples = [x for x in header if x in list(index[args.idName])]
+        samples = []
+        for idName in idNameLST:
+            samples = samples + [x for x in header if x in list(index[idName])]
+        samples = list(set(samples))
         # Subset by samples found in header
-        overlapFrame = index[index[args.idName].isin(samples)]
+        overlapFrame = index
+        for idName in idNameLST:
+            overlapFrame = overlapFrame[overlapFrame[idName].isin(samples)]
         # Get only a list of the accessions and replace the values in the pandas frame
         # header to be the accessions.
-        accessions = list(overlapFrame[args.accName])
+        accessions = overlapFrame
+        for accName in accNameLST:
+            accessions = list(overlapFrame[accName])
         header=header[0:abs(len(accessions)-len(header))] + accessions
         rawFile.columns = header
         # Write out the file
