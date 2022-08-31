afda54f697bddf39dc0db734ead47cd54e612010
charlesgregory <charles.gregory940@topper.wku.edu>
2022-08-31 11:05:00 -0400

    actually fix #16

--- libmucor/source/libmucor/atomize/package.d
+++ libmucor/source/libmucor/atomize/package.d
@@ -75,6 +75,7 @@ void parseVCF(string fn, int threads, bool multiSample, bool multiAllele, string
                 vcf_row_count++;
                 for (auto i = 0; i < recIR.hdrInfo.samples.length; i++)
                 {
+                    if(recIR.fmt.bySample[i].isNull) continue;
                     auto samIR = VcfRecSingleSample(recIR, i);
                     ser.putRecord(samIR);
                     output_count++;
@@ -90,6 +91,7 @@ void parseVCF(string fn, int threads, bool multiSample, bool multiAllele, string
                 vcf_row_count++;
                 for (auto i = 0; i < recIR.hdrInfo.samples.length; i++)
                 {
+                    if(recIR.fmt.bySample[i].isNull) continue;
                     auto samIR = VcfRecSingleSample(recIR, i);
                     for (auto j = 0; j < samIR.alt.length; j++)
                     {
